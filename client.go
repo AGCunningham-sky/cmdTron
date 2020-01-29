7e96d3b92dfd9ee5072f104022a98c06d675bac3
Adam Cunningham <adam.cunningham@sky.uk>
2020-01-29 13:33:37 +0000

    multiplayer attempt1

--- client.go
+++ client.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"github.com/danicat/simpleansi"
+	"golang.org/x/net/websocket"
 	"log"
 	"os"
 	"os/exec"
@@ -23,21 +25,42 @@ type sprite struct {
 	here 	bool
 }
 
+type Message struct {
+	PlayerA 	player `json:"playerA"`
+	PlayerB 	player `json:"playerB"`
+}
+
+type inComing struct {
+	Player	string `json:"player"`
+	Command string	`json:"command"`
+}
+
 var (
 	maze []string
 	PlayerA player
 	PlayerB player
 	maxLength = 150
 	exit bool
+	port = flag.String("port", "9000", "port used for ws connection")
+	serverIP = "10.190.159.32"
 )
 
 func main() {
 	initialise()
 	defer cleanup()
 
+	flag.Parse()
+
+	// connect
+	ws, err := connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
 	exit = false
 
-	err := loadMaze("maze.txt")
+	err = loadMaze("maze.txt")
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -55,6 +78,21 @@ func main() {
 		}
 	}(input)
 
+	// receive
+	var m Message
+	go func() {
+		for {
+			err := websocket.JSON.Receive(ws, &m)
+			if err != nil {
+				fmt.Println("Error receiving message: ", err.Error())
+				break
+			}
+			fmt.Println("Message: ", m)
+			PlayerA = m.PlayerA
+			PlayerB = m.PlayerB
+		}
+	}()
+
 	for {
 		printScreen()
 
@@ -64,7 +102,15 @@ func main() {
 				color.Cyan("Game exited")
 				exit = true
 			}
-			// Send move
+			m := inComing{
+				Player: "A",
+				Command: inp,
+			}
+			err = websocket.JSON.Send(ws, m)
+			if err != nil {
+				fmt.Println("Error sending message: ", err.Error())
+				break
+			}
 		default:
 		}
 		if exit {
@@ -72,6 +118,21 @@ func main() {
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
+}
+
+// connect connects to the local chat server at port <port>
+func connect() (*websocket.Conn, error) {
+	return websocket.Dial(fmt.Sprintf("ws://"+serverIP+":%s", *port), "", mockedIP())
+}
+
+// mockedIP is a demo-only utility that generates a random IP address for this client
+func mockedIP() string {
+	var arr [4]int
+	for i := 0; i < 4; i++ {
+		rand.Seed(time.Now().UnixNano())
+		arr[i] = rand.Intn(256)
+	}
+	return fmt.Sprintf("http://%d.%d.%d.%d", arr[0], arr[1], arr[2], arr[3])
 }
 
 func readInput() (string, error) {
@@ -139,32 +200,6 @@ func printScreen() {
 	}
 
 	simpleansi.MoveCursor(len(maze), 0)
-}
-
-func loadMaze(file string) error {
-	f, err := os.Open(file)
-	if err != nil{
-		return err
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		maze = append(maze, scanner.Text())
-	}
-
-	for row, line := range maze {
-		for col, chr := range line {
-			switch chr {
-			case 'a':
-				PlayerA.Player = append(PlayerA.Player, sprite{row, col, true})
-			case 'b':
-				PlayerB.Player = append(PlayerB.Player, sprite{row, col, true})
-			}
-		}
-	}
-
-	return nil
 }
 
 func initialise() {
--- server.go
+++ server.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"flag"
+	"fmt"
+	"golang.org/x/net/websocket"
+	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/fatih/color"
@@ -17,46 +24,172 @@ type sprite struct {
 	here 	bool
 }
 
+type Message struct {
+	PlayerA 	player `json:"playerA"`
+	PlayerB 	player `json:"playerB"`
+}
+
+type inComing struct {
+	Player	string `json:"player"`
+	Command string	`json:"command"`
+}
+
+type hub struct {
+	clients          map[string]*websocket.Conn
+	addClientChan    chan *websocket.Conn
+	removeClientChan chan *websocket.Conn
+	broadcastChan    chan Message
+}
+
 var (
 	maze []string
 	PlayerA player
 	PlayerB player
 	maxLength = 150
-	exit bool
+
+	port = flag.String("port", "9000", "port used for ws connection")
 )
 
 func main() {
-	exit = false
+	loadMaze("maze.txt")
+
+	flag.Parse()
+	log.Fatal(server(*port))
+}
+
+func updateLogic(PlayerA, PlayerB player, inp string) (crash bool, Player1, Player2 player){
+	// Update the direction
+	Player1, Player2 = playerDirection(PlayerA, PlayerB, inp)
+
+	// Calculate Movement
+	if PlayerA, crash = playerMovement(PlayerA); crash {
+		color.Blue("WASD Wins")
+		return
+	}
+	if PlayerB, crash = playerMovement(PlayerB); crash {
+		color.Red("Arrows Wins")
+		return
+	}
+
+	// Calculate winners
+	if collisionDetection(PlayerA, PlayerB.Player[0]) {
+		color.Red("Arrows Wins")
+		return
+	}
+	if collisionDetection(PlayerB, PlayerA.Player[0]) {
+		color.Blue("WASD Wins")
+		return
+	}
+
+	return
+}
+
+// server creates a websocket server at port <port> and registers the sole handler
+func server(port string) error {
+	h := newHub()
+	mux := http.NewServeMux()
+	mux.Handle("/", websocket.Handler(func(ws *websocket.Conn) {
+		handler(ws, h)
+	}))
+
+	s := http.Server{Addr: ":" + port, Handler: mux}
+	return s.ListenAndServe()
+}
+
+// handler registers a new chat client conn;
+// It runs the hub, adds the client to the connection pool
+// and broadcasts received message
+func handler(ws *websocket.Conn, h *hub) {
+	go h.run()
+
+	h.addClientChan <- ws
 
 	for {
-		//TODO: recieve data from each side & calculate updated positions
-
-		var crash bool
-		if PlayerA, crash = playerMovement(PlayerA); crash {
-			color.Blue("WASD Wins")
-			break
-		}
-		if PlayerB, crash = playerMovement(PlayerB); crash {
-			color.Red("Arrows Wins")
-			break
+		var m inComing
+		err := websocket.JSON.Receive(ws, &m)
+		if err != nil {
+			h.broadcastChan <- Message{"ERROR",err.Error()}
+			h.removeClient(ws)
+			return
 		}
 
-		if collisionDetection(PlayerA, PlayerB.Player[0]) {
-			color.Red("Arrows Wins")
-			break
-		}
-		if collisionDetection(PlayerB, PlayerA.Player[0]) {
-			color.Blue("WASD Wins")
-			break
-		}
+		//TODO: Currently it doesn't matter who send the command bceause you still use wasd v arrows
+		updateLogic(PlayerA, PlayerB, m.Command)
 
-		PlayerA, PlayerB = playerDirection(PlayerA, PlayerB, inp)
-
-		if exit {
-			break
-		}
-		time.Sleep(100 * time.Millisecond)
+		h.broadcastChan <- Message{PlayerA:PlayerA, PlayerB:PlayerB}
 	}
+}
+
+// newHub returns a new hub object
+func newHub() *hub {
+	return &hub{
+		clients:          make(map[string]*websocket.Conn),
+		addClientChan:    make(chan *websocket.Conn),
+		removeClientChan: make(chan *websocket.Conn),
+		broadcastChan:    make(chan Message),
+	}
+}
+
+// run receives from the hub channels and calls the appropriate hub method
+func (h *hub) run() {
+	for {
+		select {
+		case conn := <-h.addClientChan:
+			h.addClient(conn)
+		case conn := <-h.removeClientChan:
+			h.removeClient(conn)
+		case m := <-h.broadcastChan:
+			// This is what's put on the broadcast channel
+			h.broadcastMessage(m)
+		}
+	}
+}
+
+// removeClient removes a conn from the pool
+func (h *hub) removeClient(conn *websocket.Conn) {
+	delete(h.clients, conn.LocalAddr().String())
+}
+
+// addClient adds a conn to the pool
+func (h *hub) addClient(conn *websocket.Conn) {
+	h.clients[conn.RemoteAddr().String()] = conn
+}
+
+// broadcastMessage sends a message to all client conns in the pool
+func (h *hub) broadcastMessage(m Message) {
+	for _, conn := range h.clients {
+		err := websocket.JSON.Send(conn, m)
+		if err != nil {
+			fmt.Println("Error broadcasting message: ", err)
+			return
+		}
+	}
+}
+
+func loadMaze(file string) error {
+	f, err := os.Open(file)
+	if err != nil{
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		maze = append(maze, scanner.Text())
+	}
+
+	for row, line := range maze {
+		for col, chr := range line {
+			switch chr {
+			case 'a':
+				PlayerA.Player = append(PlayerA.Player, sprite{row, col, true})
+			case 'b':
+				PlayerB.Player = append(PlayerB.Player, sprite{row, col, true})
+			}
+		}
+	}
+
+	return nil
 }
 
 func collisionDetection(user player, opp sprite) bool {
